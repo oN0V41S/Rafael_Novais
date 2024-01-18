3e6119fbf500eaea0de4a67cb5081f5a66a1a2fd
oN0V41S <rafaelaugustonnovais@gmail.com>
2024-01-18 02:24:07 -0300

    update responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../styles/projects/projects.css'
 import tools from '../static/icons8-tools-90.png'
+import DevWizard from '../static/projects/devwizards.png'
 
 export default function Projects() {
     return (
         <section>
             <h3 className='project-text'>Veja a Seguir alguns projetos que estou desenvolvendo ou já desenvolvi.</h3>
             <div className='projects'>
-                <ItemProject desc="Projeto Desenvolvido em uma Squad em Sala de Aula com foco em práticar todo conteúdo estudado no semestre"/>
+                <ItemProject name="DevWizards" tech="ReactJS, TailwindCSS, Figma, Trello" desc="Projeto Desenvolvido em uma Squad em Sala de Aula com foco em práticar todo conteúdo estudado no semestre" img={DevWizard} />
                 <EmptyItemProject />
                 <EmptyItemProject />
                 <EmptyItemProject />
@@ -16,19 +17,23 @@ export default function Projects() {
     )
 }
 
-function ItemProject({img, desc}) {
+function ItemProject({ img, desc, tech, name }) {
     return (
         <div className='project'>
-            <img src={img} alt='' className='project-img'/>
-            <div className='project-info'>{desc}</div>
+            <img src={img} alt='' className='project-img' />
+            <div className='project-aux'>{name}</div>
+            <div className='project-info'>
+                <h4 >{desc}</h4>
+                <h5>Algumas tecnologias utilizadas: {tech}</h5>
+            </div>
         </div>
     );
 }
 
-function EmptyItemProject({}) {
+function EmptyItemProject({ }) {
     return (
         <div className='project'>
-            <img src={tools} alt='' className='project-img'/>
+            <img src={tools} alt='' className='project-empty' />
             <div className='project-name'>Em breve</div>
         </div>
     );
