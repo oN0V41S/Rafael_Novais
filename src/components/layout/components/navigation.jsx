6ff17fdf3d7bc0d12a4fbcf1f26c3e47eaea8116
oN0V41S <rafaelaugustonnovais@gmail.com>
2024-01-30 11:56:38 -0300

    up: add images
    add images skills

--- src/components/layout/components/navigation.jsx
+++ src/components/layout/components/navigation.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import home from '@/static/aside/nav/home.png'
+import tools from '@/static/aside/nav/tools.png'
+import projects from '@/static/aside/nav/project.png'
 
 export default function Navigation() {
   return (
         <ul id='Navigation' className='aside-list aside-component'>
           <li> <NavLink name="Home" href='/' src={home} /> </li>
-          <li> <NavLink name="Habilidades" href='/skills' src='' /> </li>
-          <li> <NavLink name="Projetos" href='/projects' src=''/> </li>
+          <li> <NavLink name="Habilidades" href='/skills' src={tools} /> </li>
+          <li> <NavLink name="Projetos" href='/projects' src={projects}/> </li>
         </ul>
   )
 }
--- src/components/layout/components/topSkill.jsx
+++ src/components/layout/components/topSkill.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
 
+import reactIcon from "@/static/skills/reactjs.png"
+import jSIcon from "@/static/skills/javascript.png"
+import javaIcon from "@/static/skills/java.png"
+
 export default function TopSkill(){
     return(
         <Link className="topSkill aside-component" to="/skills">
             <h3>Top Habilidades</h3>
             <ul className="topSkill-list aside-list">
-                <li><Skill src="" name="React.JS" /></li>
-                <li><Skill src="" name="JavaScript" /></li>
-                <li><Skill src="" name="java" /></li>
+                <li><Skill src={reactIcon} name="React.JS" /></li>
+                <li><Skill src={jSIcon} name="JavaScript" /></li>
+                <li><Skill src={javaIcon} name="java" /></li>
             </ul>
         </Link>
     )
@@ -16,7 +20,7 @@ export default function TopSkill(){
 function Skill({src, name}){
     return(
         <div className="topSkill-item aside-item" >
-            <img src={src} alt="imgSkill"/>
+            <img src={src} alt="imgSkill" className="aside-img"/>
             <h5>{name}</h5>
         </div>
     )
