9c003d969e94710f85c80a2d76885db7b47c59dc
Novais Rafael <rafaelaugustonnovais@gmail.com>
2025-02-26 20:14:34 +0000

    update(db): remake and add some technical skills

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,40 @@
 import Layout from "@/components/layout/layout";
 
-import Project from "@/components/projects/project"
-
 export default function Projects() {
-  return (
-      <Layout>
-        <Project/>
-      </Layout>
-  );
-}
+  const projectsList = db.projects;
+   return (
+    <Layout>
+      <main>
+        <h1 className="title-page">Projetos</h1>
+        <ul className="project-list">
+          {projectsList.map((projectItem) => (
+            <ItemProject
+              name={projectItem.name}
+              tech={projectItem.tech}
+              desc={projectItem.desc}
+              href={projectItem.href}
+            />
+          ))}
+        </ul>
+      </main>
+    </Layout>
+    );
+  }
+  
+  const ItemProject = ({ img, desc, tech, name, href }) => {
+    return (
+      <Link className="project-item" to={href}>
+        <h1>{name}</h1>
+        <div>
+          <p>{desc}</p>
+          <p>
+            {" "}
+            <strong>Tecnologias e Ferramentas</strong> <br />
+            {tech}
+          </p>
+        </div>
+        <h3>Clique para abrir o Github</h3>
+      </Link>
+    );
+  };
+  
